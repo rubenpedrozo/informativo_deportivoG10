73fd9f76a98065ac8d9c88383f7305fe4a5398a2
rubenpedrozo <rubenpedrozo8@gmail.com>
2022-11-09 15:37:17 -0500

    Corrección saludos

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send('😍 Hola Beatifull Family los amo 😘')
+    res.send('😍 Saludos desde BECERRIL😘')
 })
 
 app.listen(8000,()=>{
